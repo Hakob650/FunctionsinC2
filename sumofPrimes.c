c1a54502435734445daa913fa8b85265e869cc79
Hakob650 <hakob11meneshian@gmail.com>
2025-04-05 16:41:44 +0400

    Add files via upload

---
+++ sumofPrimes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int isPrime(int num){
+        if(num<=1){
+                return 0;
+        }if(num==2){
+                return 1;
+        }if(num%2==0){
+                return 0;
+        }for(int i=3;i*i<num;++i){
+                if(num%i==0){
+                        return 0;
+                }
+        }return 1;
+}
+
+int sumofPrimes(int num){
+	for(int i=2;i<=num/2;++i){
+		if(isPrime(i)&&isPrime(num-i)){
+			return 1;
+		}
+	}return 0;
+}
