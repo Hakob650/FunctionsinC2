ea03003ad8430a90a485f7de1c953bb9b233a917
Hakob650 <hakob11meneshian@gmail.com>
2025-04-05 16:40:04 +0400

    Add files via upload

---
+++ main4.c
@@ -0,0 +1,9 @@
+int sumofDigits(int num);
+#include <stdio.h>
+int main(){
+	int num;
+	printf("enter num");
+	scanf("%d",&num);
+	printf("sum of digits is %d\n",sumofDigits(num));
+        return 0;
+}	
