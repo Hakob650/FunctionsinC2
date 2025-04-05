345b8096ef7fb50f55a4b840962e9af7ced49ede
Hakob650 <hakob11meneshian@gmail.com>
2025-04-05 16:42:29 +0400

    Add files via upload

---
+++ main6.c
@@ -0,0 +1,13 @@
+int power(int num,int exp);
+#include <stdio.h>
+int main(){
+	int num;
+	printf("mutqagreq tivy");
+	scanf("%d",&num);
+	int exp;
+	printf("mutqagreq astichany");
+	scanf("%d",&exp);
+	int result=power(num,exp);
+	printf("%d-i %d astichany  %d\n e",num,exp,result);
+	return 0;
+}
