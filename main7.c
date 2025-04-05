b4d5cf0d4d556e2d6b69853a30e748583ed1af97
Hakob650 <hakob11meneshian@gmail.com>
2025-04-05 16:43:10 +0400

    Add files via upload

---
+++ main7.c
@@ -0,0 +1,10 @@
+int reverse(int num);
+#include <stdio.h>
+int main(){
+	int num;
+	printf("mutqagreq tivy");
+	scanf("%d",&num);
+	int result=reverse(num);
+	printf("reversed number:  %d",result);
+	return 0;
+}
